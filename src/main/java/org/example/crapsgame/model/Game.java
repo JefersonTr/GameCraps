8ec31274ddbbbc10ee761823a910abbeddf5d69d
Lady Matabanchoy <lady.matabanchoy@correounivalle.edu.co>
2024-10-08 20:06:37 -0500

    first commit

--- src/main/java/org/example/crapsgame/model/Game.java
+++ src/main/java/org/example/crapsgame/model/Game.java
@@ -3,7 +3,8 @@ package org.example.crapsgame.model;
 public class Game {
     private int shootCount;
     private int shoot;
-    private int point;
+    private int point_win;
+    private int point_lose;
     private boolean win;
     private boolean lose;
     Dice dice1, dice2;
@@ -11,7 +12,11 @@ public class Game {
     public Game() {
         this.shootCount = 0;
         this.shoot = 0;
-        this.point = 0;
+        this.point_win = 0;
+        this.point_lose = 0;
+        this.win = false;
+        this.lose = false;
+
     }
 
     public int getShoot() {
@@ -19,23 +24,41 @@ public class Game {
     }
 
     public int getShootCount() {
+        //System.out.println(shootCount);
         return this.shootCount;
     }
 
-    public int getPoint() {
-        return this.point;
+    public int getPointWin() {
+        //System.out.println(point);
+        return this.point_win;
     }
 
+    public int getPointLose() {
+        return this.point_lose;
+    }
     public boolean isWin() {
-        return this.win;
+        this.point_win++;
+        return this.win == true;
     }
 
     public boolean isLose() {
-        return this.lose;
+        this.point_lose++;
+        return this.lose == true;
     }
 
-    public int rollDices() {
+    public void resetGame() {
+        this.shootCount = 0;
+        this.shoot = 0;
+        this.win = false;
+        this.lose = false;
+    }
+
+    public int rollDices(int dice1Value, int dice2Value) {
+        shoot = dice1Value + dice2Value; //Se obtiene el valor de dados
+        //System.out.println(shoot);
+        this.shootCount++; //Se incrementa contador de tiros
         return this.shoot;
     }
+
 
 }
